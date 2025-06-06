3de91baab1c04b94777fe9a69bd69d4da1baec86
Roshan Gupta <lucysigma72@gmail.com>
2025-06-06 19:34:11 +0545

    it includes all routing and the meta information like title , description which are passed as object to index and about ejs

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,8 +3,16 @@ const router = express.Router();
 
 //Routes
 
+const locals = {
+  title: "Daily Blogs",
+  description: "Simple Blog App Created with NodeJs, ExpressJs & MongoDB.",
+};
 router.get("", (req, res) => {
-  res.send("Hello world");
+  res.render("index", { locals }); // it render the index.ejs file as home page
+});
+
+router.get("/about", (req, res) => {
+  res.render("about", { locals });
 });
 
 module.exports = router;
